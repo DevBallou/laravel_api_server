be310fb6f5493d3823bd77048f03b4b0b40135ef
BAALLOU Hicham <baallou1996h@gmail.com>
2024-03-06 14:00:09 +0100

    RESTful API RouteDesign and Laravel Routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +17,40 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('user', 'user');
+// Route::controller(AuthController::class)->group(function () {
+//     Route::post('login', 'login');
+//     Route::post('register', 'register');
+//     Route::post('logout', 'logout');
+//     Route::post('user', 'user');
+// });
+
+Route::get('/users', function (Request $request) {
+    dump($request);
+    return new JsonResponse([
+        'data' => 'aaaaa'
+    ]);
+});
+
+Route::get('/users/{user}', function (\App\Models\User $user) {
+    return new JsonResponse([
+        'data' => $user
+    ]);
+});
+
+Route::post('/users', function () {
+    return new JsonResponse([
+        'data' => 'posted'
+    ]);
+});
+
+Route::patch('/users/{user}', function (\App\Models\User $user) {
+    return new JsonResponse([
+        'data' => 'patched'
+    ]);
+});
+
+Route::delete('/users/{user}', function (\App\Models\User $user) {
+    return new JsonResponse([
+        'data' => 'deleted'
+    ]);
 });
